f22b8430e49b4bf036620408781e364e40fbd3a6
Nick Betteridge <nick.betteridge@gmail.com>
2024-04-12 13:30:04 +0100

    wip

--- lib/handshake_server13.mli
+++ lib/handshake_server13.mli
@@ -5,6 +5,6 @@ val answer_client_hello: ?embed_quic_transport_params:(Cstruct.t option -> Cstru
     hrr:bool ->
     handshake_state ->
     client_hello ->
-    nonce -> (handshake_return, failure) result
+    Cstruct.t -> (handshake_return, failure) result
 
 val handle_handshake : ?embed_quic_transport_params:(Cstruct.t option -> Cstruct.t option) -> server13_handshake_state -> handshake_state -> Cstruct.t -> (handshake_return, failure) result
