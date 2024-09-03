075a82caebea82a3b315fcbeb295fabc8219d9e4
Nick Betteridge <nick.betteridge@gmail.com>
2024-09-03 17:24:29 +0100

    wip

--- lib/engine.ml
+++ lib/engine.ml
@@ -142,7 +142,7 @@ let encrypt (version : tls_version) (st : crypto_state) ty buf =
               in
               (AEAD c, msg)
         in
-        (Some { sequence = Int64.succ ctx.sequence ; cipher_st = c_st }, ty, enc)
+        (Some { sequence = Int64.succ ctx.sequence ; cipher_st = c_st; traffic_secret = ctx.traffic_secret }, ty, enc)
 
 (* well-behaved pure decryptor *)
 let verify_mac sequence mac mac_k ty ver decrypted =
@@ -272,7 +272,7 @@ let decrypt ?(trial = false) (version : tls_version) (st : crypto_state) ty buf 
        Error (`Fatal (`Handshake (`Message msg))))
   | Some ctx, _ ->
     let* st', msg = dec ctx in
-    let ctx' = { cipher_st = st' ; sequence = Int64.succ ctx.sequence } in
+    let ctx' = { cipher_st = st' ; sequence = Int64.succ ctx.sequence; traffic_secret = ctx.traffic_secret } in
     Ok (Some ctx', msg, ty)
 
 (* party time *)
